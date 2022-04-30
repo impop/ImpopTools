69c6fe3e24e89c286244c630e7639ed4740684a3
Miquel Parejo <miquel.parejo@allianz.ie>
2022-04-30 08:02:39 +0100

    added bravia set mode commands

---
+++ braviaCommands/SetGraphicsMode.bat
@@ -0,0 +1,13 @@
+rem https://gist.github.com/kalleth/e10e8f3b8b7cb1bac21463b0073a65fb
+
+curl http://192.168.1.115/sony/video ^
+-H "Content-Type: application/json" ^
+-H "X-Auth-PSK: 1234" ^
+-X POST ^
+-d @graphicsmode.json
+
+curl http://192.168.1.115/sony/video ^
+-H "Content-Type: application/json" ^
+-H "X-Auth-PSK: 1234" ^
+-X POST ^
+-d @autoPictureModeOn.json
