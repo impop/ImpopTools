c2231c6b082d65223e25d0c5b704fcc304db431c
Miquel Parejo <miquel.parejo@allianz.ie>
2022-11-23 11:44:53 +0000

    added sports mode and modified cinema

--- braviaCommands/SetCinemaHoneMode.bat
+++ braviaCommands/SetCinemaHoneMode.bat
@@ -2,10 +2,23 @@ curl http://192.168.1.115/sony/video ^
 -H "Content-Type: application/json" ^
 -H "X-Auth-PSK: 1234" ^
 -X POST ^
+-d @autoPictureModeOff.json
+
+curl http://192.168.1.115/sony/video ^
+-H "Content-Type: application/json" ^
+-H "X-Auth-PSK: 1234" ^
+-X POST ^
 -d @cinemaHomemode.json
 
 curl http://192.168.1.115/sony/video ^
 -H "Content-Type: application/json" ^
 -H "X-Auth-PSK: 1234" ^
 -X POST ^
--d @autoPictureModeOn.json
+-d @hdrModeOff.json
+
+curl http://192.168.1.115/sony/video ^
+-H "Content-Type: application/json" ^
+-H "X-Auth-PSK: 1234" ^
+-X POST ^
+-d @hdrModeAuto.json
+
--- braviaCommands/SetGraphicsMode.bat
+++ braviaCommands/SetGraphicsMode.bat
@@ -8,4 +8,16 @@ curl http://192.168.1.115/sony/video ^
 -H "Content-Type: application/json" ^
 -H "X-Auth-PSK: 1234" ^
 -X POST ^
+-d @hdrModeOff.json
+
+curl http://192.168.1.115/sony/video ^
+-H "Content-Type: application/json" ^
+-H "X-Auth-PSK: 1234" ^
+-X POST ^
 -d @autoPictureModeOn.json
+
+curl http://192.168.1.115/sony/video ^
+-H "Content-Type: application/json" ^
+-H "X-Auth-PSK: 1234" ^
+-X POST ^
+-d @hdrModeAuto.json
